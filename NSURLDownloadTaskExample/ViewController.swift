bbae7d5a4966ce646ce7e969901635fafc4b6551
serendipityapps <info@serendipityappcompany.com>
2015-07-14 20:58:21 +0100

    Initial Commit

---
+++ NSURLDownloadTaskExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NSURLDownloadTaskExample
+//
+//  Created by David Roberts on 14/07/2015.
+//  Copyright © 2015 Dave Roberts. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
