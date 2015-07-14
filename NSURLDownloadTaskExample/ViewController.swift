ccf3cf3d8fcda1eb77ef969e58e5e3ceac2b446b
serendipityapps <info@serendipityappcompany.com>
2015-07-14 21:19:55 +0100

    Added Methods

--- NSURLDownloadTaskExample/ViewController.swift
+++ NSURLDownloadTaskExample/ViewController.swift
@@ -13,7 +13,56 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let paths = NSSearchPathForDirectoriesInDomains(.DesktopDirectory, .UserDomainMask, true)
+
+        if let desktopPath = paths.first {
+            
+            let filePathToCopyTo = desktopPath.stringByAppendingPathComponent("downloadedFile.pdf")
+            
+            downloadFile("http://devstreaming.apple.com/videos/wwdc/2015/711y6zlz0ll/711/711_networking_with_nsurlsession.pdf?dl=1", locationToCopyToPath:filePathToCopyTo)
+        }
+    }
+    
+    func downloadFile(fileToDownload: String, locationToCopyToPath:String) {
+        
+        let url = NSURL(string: fileToDownload)
+        let urlToCopyTo = NSURL(fileURLWithPath: locationToCopyToPath)  // NOTE: fileURLWithPath initializer for local file path
+
+        if let unwrappedURL = url {
+            
+            let downloadTask = NSURLSession.sharedSession().downloadTaskWithURL(unwrappedURL) { (urlToCompletedFile, reponse, error) -> Void in
+                
+                // unwrap error if present
+                if let unwrappedError = error {
+                    print(unwrappedError)
+                }
+                else {
+                    
+                    if let unwrappedURLToCachedCompletedFile = urlToCompletedFile {
+                        
+                        print(unwrappedURLToCachedCompletedFile)
+                        
+                        // Swift 2
+                        do {
+                         try NSFileManager.defaultManager().moveItemAtURL(unwrappedURLToCachedCompletedFile, toURL: urlToCopyTo)
+                        }
+                        catch let error {
+                            print(error)
+                        }
+                        
+                        // Swift 1.2
+                        // NSFileManager.defaultManager().moveItemAtURL(unwrappedURLToCachedCompletedFile, toURL: urlToCopyTo, error:nil)
+                        
+                        // Careful here this moving / copying can fail and give errors - i.e if file already present, can be dealt with by using delegate of NSFileManager such as: -
+                        
+                        // optional func fileManager(fileManager: NSFileManager, shouldMoveItemAtURL srcURL: NSURL, toURL dstURL: NSURL) -> Bool
+
+                    
+                    }
+                }
+            }
+            downloadTask?.resume()
+        }
     }
 
     override var representedObject: AnyObject? {
